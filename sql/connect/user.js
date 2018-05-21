2c14b0e33b7f9a96a2c1da3b871cfbaf0379fad5
Tianfer <975206177@qq.com>
2018-05-22 00:43:57 +0800

    删除console

--- sql/connect/user.js
+++ sql/connect/user.js
@@ -3,6 +3,5 @@ const connection = require('../config')
 exports.getUserInfo = async (id) => {
   let sql = `select name from teacher where teacher_id=${id}`
   const result = await connection.getResult(sql)
-  console.log(result)
   return result
 }
