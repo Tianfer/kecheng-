4b6b08b6ffe9d434520f57119c2b6ab67c260333
Tianfer <975206177@qq.com>
2018-05-22 01:40:07 +0800

    将只搜索姓名改为搜索全部

--- sql/connect/user.js
+++ sql/connect/user.js
@@ -1,7 +1,7 @@
 const connection = require('../config')
 
 exports.getUserInfo = async (id) => {
-  let sql = `select name from teacher where teacher_id=${id}`
+  let sql = `select * from teacher where teacher_id=${id}`
   const result = await connection.getResult(sql)
   return result
 }
